6352152125d75f2ebf43bbe71ee94eaff56afb9b
demogi4523@ <demogi4523@yandex.ru>
2022-03-29 07:57:51 +0800

    2 tutorial

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
   return render(req, 'chat/index.html')
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
